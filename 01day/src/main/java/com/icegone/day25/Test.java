f032e003b929651c0d96923f28efb73f6c524e5b
bjchen <1725781624@qq.com>
2020-07-23 17:14:39 +0800

    hutool部分对象实例化

---
+++ 01day/src/main/java/com/icegone/day25/Test.java
@@ -0,0 +1,21 @@
+package com.icegone.day25;
+
+import cn.hutool.system.*;
+import cn.hutool.system.oshi.OshiUtil;
+
+/**
+ * @program: learning
+ * @description: 测试 hutool工具包
+ * @author: bjchen
+ * @create: 2020-07-22
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+        //System.out.println(OshiUtil.getCpuInfo().toString());
+        System.out.println(new HostInfo().toString());
+        System.out.println(new JavaInfo().toString());
+        System.out.println(new JavaRuntimeInfo().toString());
+        System.out.println(new JvmSpecInfo().toString());
+    }
+}
